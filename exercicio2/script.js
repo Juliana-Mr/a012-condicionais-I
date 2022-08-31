1c055c08a8e83e65129ef8476212cfdb66a0e1e9
Juliana <juliana-marques-@hotmail.com>
2022-08-31 14:33:41 -0300

    Exercício 12 feito

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,23 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
+let idade = 29
+let ensinoMedioCompleto = true
+let isCursandoOutraFaculdade = false
 
-if(){
-
+if(idade >= 18){
+    console.log('A pessoa é maoir de idade')
 }else{
-	
+    console.log('A pessoa é menor de idade')	
 }
+
+if(ensinoMedioCompleto){
+    console.log('A pessoa completou o ensino médio')
+}else{
+    console.log('A pessoa NÃO completou o ensino médio')
+}
+
+if(isCursandoOutraFaculdade){
+    console.log('A pessoa está cursando uma faculdade')
+}else{
+    console.log('A pessoa não está cursando uma faculdade')
+}
+
+
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,15 @@
-let nacionalidade //= adicione o prompt aqui
+let nacionalidade = prompt('Escreva aqui sua nacionalidade').toLowerCase()
+
+if(nacionalidade === 'brasileira' || nacionalidade === 'brasileiro'){
+    console.log('A pessoa é brasileira')
+}else if(nacionalidade === 'argentina' || nacionalidade === 'argentino'){
+     console.log('A pessoa é argentina')
+}else if(nacionalidade === 'uruguaia' || nacionalidade === 'uruguaio'){
+     console.log('A pessoa é uruguaia')
+}else if(nacionalidade === 'chilena' || nacionalidade === 'chileno'){
+     console.log('A pessoa é chilena')
+ }else if(nacionalidade === 'colombiana' || nacionalidade === 'colombiano'){
+     console.log('A pessoa é colombiana')
+ }else{    
+     console.log('Nacionalidade não encontrada')
+ }
